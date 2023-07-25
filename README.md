3a52e9b841101b62d1b348f2c5c93dc7947e0e6b
mrkvon <mrkvon@protonmail.com>
2023-07-25 18:41:41 +0300

    Add README, project name, and first experiment with notifications

--- README.md
+++ README.md
@@ -1,26 +1,28 @@
-# Node TypeScript starter
+# Solid email notifications
 
-This is a repository to bootstrap node and TypeScript project quickly. It has prettier and eslint set up, and you can use .env file to provide environment variables.
+This service sends an email when a new notification arrives to your Solid Pod inbox
 
-The setup is opinionated, as author prefers it. Maybe you'll like it, too...
+## How it will work
 
-## Prerequisities
+- It will be a bot agent and it will run on some server
+- You'll have to give it Read access to your inbox (how does bot get an access?)
+- You'll have to tell it where to send you emails
+- It'll subscribe to Solid [webhook](https://solid.github.io/notifications/webhook-channel-2023) [notifications](https://solidproject.org/TR/notifications-protocol) of your inbox
+- When a notification arrives to your inbox, this agent will get notified by the webhook. It will check whether it's a notification about something specific (in the context of sleepy.bike e.g. message or contact request), it'll compose the message and send it to you
+- It will regularly re-subscribe to your inbox because some Solid pods drop the subscription regularly
+- If your pod doesn't support webhook notifications, maybe this bot will check your inbox regularly (a few times per day) and send you a notification when it finds something new there
 
-Node (v16+) and yarn installed
+## Usage (WIP)
 
-## Installation
+If you want to run the current experiments, you need to install [CommunitySolidServer](https://github.com/CommunitySolidServer/CommunitySolidServer) and run it with option `--seededPodConfigJson path/to/experimental-pod-seed.json`
 
-1. clone this repository `git clone https://github.com/mrkvon/node-typescript-starter.git name-of-your-project`
-2. go to project folder `cd name-of-your-project`
-3. install node modules `yarn`
-4. rename project in [`package.json`](./package.json)
-5. optional: `cp .env.sample .env` if you want to use .env for defining environment variables, like secrets etc.
-6. optional: also feel free to remove or change license (LICENSE file and entry in package.json), and this README
+Then run in other terminal
 
-## Usage
+```bash
+yarn start
+```
 
-- run `yarn start`
-- develop your project starting from `src/index.ts`
+and see some output
 
 ## License
 
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,91 @@
+import {
+  buildAuthenticatedFetch,
+  createDpopHeader,
+  generateDpopKeyPair,
+} from '@inrupt/solid-client-authn-core'
 import { config } from 'dotenv'
+import http from 'http'
+
+const server = http.createServer((req, res) => {
+  let requestBody = ''
+
+  req.on('data', chunk => {
+    requestBody += chunk
+  })
+
+  req.on('end', () => {
+    console.log('Received request body:', requestBody)
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello, world!')
+  })
+})
+
+server.listen(4000, () => {
+  console.log('Server started and listening on port 4000')
+})
 
 config()
-
-/** Here we run the code */
 ;(async () => {
-  /** This is how you use environment variables e.g. defined in .env */
-  const who: string = process.env.hello ?? 'World'
+  const response = await fetch('http://localhost:3000/idp/credentials/', {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    // The email/password fields are those of your account.
+    // The name field will be used when generating the ID of your token.
+    body: JSON.stringify({
+      email: 'bot@example',
+      password: 'password',
+      name: 'bot-token',
+    }),
+  })
 
-  console.log(
-    `Hello ${who}!\n\nRename this project and start coding from ./src/index.ts\n\nHappy developing!`,
+  const { id, secret } = await response.json()
+
+  const dpopKey = await generateDpopKeyPair()
+
+  // These are the ID and secret generated in the previous step.
+  // Both the ID and the secret need to be form-encoded.
+  const authString = `${encodeURIComponent(id)}:${encodeURIComponent(secret)}`
+  // This URL can be found by looking at the "token_endpoint" field at
+  // http://localhost:3000/.well-known/openid-configuration
+  // if your server is hosted at http://localhost:3000/.
+  const tokenUrl = 'http://localhost:3000/.oidc/token'
+  const response2 = await fetch(tokenUrl, {
+    method: 'POST',
+    headers: {
+      // The header needs to be in base64 encoding.
+      authorization: `Basic ${Buffer.from(authString).toString('base64')}`,
+      'content-type': 'application/x-www-form-urlencoded',
+      dpop: await createDpopHeader(tokenUrl, 'POST', dpopKey),
+    },
+    body: 'grant_type=client_credentials&scope=webid',
+  })
+
+  // This is the Access token that will be used to do an authenticated request to the server.
+  // The JSON also contains an "expires_in" field in seconds,
+  // which you can use to know when you need request a new Access token.
+  const { access_token: accessToken } = await response2.json()
+
+  // The DPoP key needs to be the same key as the one used in the previous step.
+  // The Access token is the one generated in the previous step.
+  const authFetch = await buildAuthenticatedFetch(fetch, accessToken, {
+    dpopKey,
+  })
+  // authFetch can now be used as a standard fetch function that will authenticate as your WebID.
+  // This request will do a simple GET for example.
+  const response3 = await authFetch(
+    'http://localhost:3000/.notifications/WebhookChannel2023/',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        '@context': ['https://www.w3.org/ns/solid/notification/v1'],
+        type: 'http://www.w3.org/ns/solid/notifications#WebhookChannel2023',
+        topic: 'http://localhost:3000/person/profile/card',
+        sendTo: 'http://localhost:4000',
+      }),
+      headers: { 'content-type': 'application/ld+json' },
+    },
   )
+
+  console.log(response.status, await response3.text())
 })()
